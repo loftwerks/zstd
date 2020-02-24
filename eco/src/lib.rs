415316374a138c0f30c2abfd7911c4dcf07f2a59
Malachi Austin <malachiaustin02@gmail.com>
2020-02-23 20:23:16 -0500

    Updated collections and eco

--- eco/src/lib.rs
+++ eco/src/lib.rs
@@ -1,9 +1,12 @@
-#![crate_name = "eco"]
-#![warn(missing_docs)]
-#![allow(unused_imports)]
-#![allow(dead_code)]
-#![allow(clippy::needless_return)]
-#![no_std]
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(feature = "const_fn", feature(const_fn))]
+#![cfg_attr(feature = "const_fn", feature(const_in_array_repeat_expressions))]
+#![cfg_attr(feature = "inline_asm", feature(asm))]
+#![cfg_attr(feature = "abi_x86_interrupt", feature(abi_x86_interrupt))]
+#![cfg_attr(feature = "deny-warnings", deny(warnings))]
+#![cfg_attr(feature = "deny-warnings", deny(missing_docs))]
+#![cfg_attr(not(feature = "deny-warnings"), warn(missing_docs))]
+#![deny(missing_debug_implementations)]
 /*!
 # The Orion Project - ECO
 *An Engine-Component-Object implementation for the Orion Project*
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
-#![crate_name = "zstd"]
-#![warn(missing_docs)]
-#![allow(unused_imports)]
-#![allow(dead_code)]
-#![allow(clippy::needless_return)]
-#![no_std]
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(feature = "const_fn", feature(const_fn))]
+#![cfg_attr(feature = "const_fn", feature(const_in_array_repeat_expressions))]
+#![cfg_attr(feature = "inline_asm", feature(asm))]
+#![cfg_attr(feature = "abi_x86_interrupt", feature(abi_x86_interrupt))]
+#![cfg_attr(feature = "deny-warnings", deny(warnings))]
+#![cfg_attr(feature = "deny-warnings", deny(missing_docs))]
+#![cfg_attr(not(feature = "deny-warnings"), warn(missing_docs))]
+#![deny(missing_debug_implementations)]
 /*!
 # Crate zstd
 *A lightweight alternative to the Rust standard library*
